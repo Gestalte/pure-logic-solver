2107874502c4a32ad754beae56804ad80d4673d2
Gestalte <66849891+Gestalte@users.noreply.github.com>
2026-01-07 22:24:40 +0200

    Add cycling through lines.

--- PureLogicSolver.c
+++ PureLogicSolver.c
@@ -97,6 +97,25 @@ static int NumberOfGates(int level)
     return count;
 }
 
+static void CheckLineClick(Gate* gate, Texture2D* gray, Texture2D* white, Texture2D* black)
+{
+    if(gate->line.texture == gray)
+    {
+       gate->line.texture = white;
+       gate->locked = true;
+    }
+    else if(gate->line.texture == white)
+    {
+        gate->line.texture = black;
+        gate->locked = true;
+    }
+    else if(gate->line.texture == black)
+    {
+        gate->line.texture = gray;
+        gate->locked = false;
+    }
+}
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -117,10 +136,24 @@ int main(void)
     }
   
     Texture2D* andGate = &Textures[0];
+
     Texture2D* grayDown = &Textures[11];
     Texture2D* grayUp = &Textures[8];
     Texture2D* grayStraight = &Textures[21];
     Texture2D* grayFork = &Textures[15];
+    Texture2D* grayDual = &Textures[18];
+
+    Texture2D* whiteDown = &Textures[9];
+    Texture2D* whiteUp = &Textures[6];
+    Texture2D* whiteStraight = &Textures[19];
+    Texture2D* whiteFork = &Textures[12];
+    Texture2D* whiteDual = &Textures[16];
+
+    Texture2D* blackDown = &Textures[10];
+    Texture2D* blackUp = &Textures[7];
+    Texture2D* blackStraight = &Textures[20];
+    Texture2D* blackFork = &Textures[13];
+    Texture2D* blackDual = &Textures[17];
 
     int centerX = ScreenWidth/2;
     int centerY = ScreenHeight/2;
@@ -181,6 +214,7 @@ int main(void)
     }
 
     Button saveEdit = {{ScreenWidth/2-50, 30, 100, 30}, "Save"};
+    Button resetLines = {{ScreenWidth/2-160, 30, 100, 30}, "Reset"};
     Button decrementLevel = {{saveEdit.rect.x + 110, 30, 50, 30}, "-"};
     Button incrementLevel = {{decrementLevel.rect.x + 110, 30, 50, 30}, "+"};
     MyRect gateMenu = {(ScreenWidth/2) - 300, (ScreenHeight) - 80, 600, 70};
@@ -260,11 +294,61 @@ int main(void)
                     }
                 }
             }
+
+            // Check for line changes
+            for (int i = 0; i < gateCount; i++)
+            {
+                if(CheckLeftClick(&Gates[i].line.rect))
+                {
+                     CheckLineClick(&Gates[i], grayFork, whiteFork, blackFork);
+                     CheckLineClick(&Gates[i], grayUp, whiteUp, blackUp);
+                     CheckLineClick(&Gates[i], grayDown, whiteDown, blackDown);
+                     CheckLineClick(&Gates[i], grayStraight, whiteStraight, blackStraight);
+                     CheckLineClick(&Gates[i], grayDual, whiteDual, blackDual);
+                }
+            }
         }
         else
         {
-            // TODO: Check for line clicks and cycle through colour when one is found.
-            // TODO: Toggle success/failure based on inputs vs. output.
+            // Check for line changes
+            for (int i = 0; i < gateCount; i++)
+            {
+                if(CheckLeftClick(&Gates[i].line.rect) && Gates[i].locked == false)
+                {
+                    CheckLineClick(&Gates[i], grayFork, whiteFork, blackFork);
+                    CheckLineClick(&Gates[i], grayUp, whiteUp, blackUp);
+                    CheckLineClick(&Gates[i], grayDown, whiteDown, blackDown);
+                    CheckLineClick(&Gates[i], grayStraight, whiteStraight, blackStraight);
+                    CheckLineClick(&Gates[i], grayDual, whiteDual, blackDual);                
+                    Gates[i].locked = false; // CheckLineClick sets locked so set it back.
+                }
+            }
+
+            if(CheckLeftClick(&resetLines.rect))
+            {
+                for (int i = 0; i < GATE_COUNT; i++) 
+                {
+                    if(Gates[i].locked == false)
+                    {
+                        if(Gates[i].level == Gates[i].place && Gates[i].level != 1)
+                        {
+                            Gates[i].line.texture = grayUp;
+                        }
+                        else if(Gates[i].place == 1 && Gates[i].level != 1)
+                        {
+                            Gates[i].line.texture = grayDown;
+                        }
+                        else if(Gates[i].level == 1 && Gates[i].place == 1)
+                        {
+                            Gates[i].line.texture = grayStraight;
+                        }
+                        else
+                        {
+                            Gates[i].line.texture = grayFork;
+                        }
+                    }
+                }
+            }
         }
 
         // Draw
@@ -295,6 +379,10 @@ int main(void)
                     DrawTexture(Textures[i], gateMenu.x + (i * 100), gateMenu.y + 10, WHITE);
                 }
             }
+        }
+        else
+        {
+            DrawButton(&resetLines);
         }
 
         for (int i = 0; i < gateCount; i++) 
